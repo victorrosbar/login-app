c264c84a6ce3e952deebec2287554a2e9c5854da
Victor Barros <victorbarros.tec@ciclic.com.br>
2024-01-24 21:04:38 +1100

    Feature: Initial React base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LoginApp } from './Components/LoginApp/LoginApp';
+
+function App() {
+  return (
+    <div>
+      <LoginApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/LoginApp/LoginApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LoginApp.css'
+
+export const LoginApp = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
